9b43d22c1e96d2d5fe345f852a02d10bedced2b3
Anchal Shukla <anchalshuka7060153412@gmail.com>
2023-11-22 04:06:55 +0530

    dfdf

---
+++ CRM/Models/DTO/EmployeeBasicinfo.cs
@@ -0,0 +1,18 @@
+﻿namespace CRM.Models.DTO
+{
+    public class EmployeeBasicinfo
+    {
+        public int Id { get; set; }
+        public string? PersonalEmailAddress { get; set; }
+        public decimal? MobileNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int? Age { get; set; }
+        public string FatherName { get; set; } = null!;
+        public string? Pan { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? City { get; set; }
+        public string? StateId { get; set; }
+        public Decimal Pincode { get; set; }
+    }
+}
