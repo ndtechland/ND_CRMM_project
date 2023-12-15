e2376867a18446b45fc1afbcaab9b50ec85c0df0
krishnaG111 <ndinfotechteam@gmail.com>
2023-12-15 13:28:10 +0530

    changes2

---
+++ CRM/Models/DTO/EmployeeList.cs
@@ -0,0 +1,20 @@
+﻿namespace CRM.Models.DTO
+{
+    public class EmployeeList
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmployeeId { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public string WorkEmail { get; set; } = null!;
+        public string GenderId { get; set; } = null!;
+        public string WorkLocationId { get; set; } = null!;
+        public string DesignationId { get; set; } = null!;
+        public string DepartmentId { get; set; } = null!;
+        public string MonthlyCTC { get; set; } = null!;
+
+        public bool? IsDeleted { get; set; }
+    }
+}
