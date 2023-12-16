72a0591f8b676df94a4d78c1dfa3a074e321daf0
ndshubham <153910931+ndshubham@users.noreply.github.com>
2023-12-16 17:37:40 +0530

    HHH

---
+++ CRM/Models/DTO/GenerateSalary.cs
@@ -0,0 +1,10 @@
+﻿namespace CRM.Models.DTO
+{
+    public class GenerateSalary
+    {
+        public long CustomerID { get; set; }
+        public long LocationID { get; set; }
+        public DateTime Month { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
