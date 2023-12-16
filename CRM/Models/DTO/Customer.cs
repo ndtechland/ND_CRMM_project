023228f947af7fc611b3cc18dce395c1449d5211
krishnaG111 <ndinfotechteam@gmail.com>
2023-12-16 17:28:21 +0530

    changes16122023

---
+++ CRM/Models/DTO/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM.Models.DTO
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? CompanyName { get; set; }
+        [NotMapped]
+        public string[] WorkLocation { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? AlternateNumber { get; set; }
+        public string? Email { get; set; }
+        public string? GstNumber { get; set; }
+        public string? BillingAddress { get; set; }
+        public string? ProductDetails { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime RenewDate { get; set; }
+    }
+}
