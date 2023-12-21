5b907da4f438af3ce85cc9a2e35b91e8db9a2076
Anchal Shukla <anchalshuka7060153412@gmail.com>
2023-12-21 10:59:49 +0530

    EPF

---
+++ CRM/Models/CRM/Employeer_EPF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class Employeer_EPF
+    {
+        public int Id { get; set; }
+
+        public string? EPF_Number { get; set; }
+        public string? Deduction_Cycle { get; set; }
+        public string? Employer_Contribution_Rate { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CRM/Models/CRM/forgotpassword.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM.Models.CRM
+{
+    public class forgotpassword
+    {
+        [Required,EmailAddress,Display(Name ="Registered email address")]
+        public string Email { get; set; }
+        public bool EmailSent { get; set; }
+    }
+}
