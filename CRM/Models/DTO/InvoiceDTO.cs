9ef2642f1426f9c097a18d07e417f6b948fcaece
ndshubham <153910931+ndshubham@users.noreply.github.com>
2023-12-22 12:13:32 +0530

    Invoice code commit

---
+++ CRM/Models/DTO/InvoiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CRM.Models.DTO
+{
+
+        public class InvoiceDTO
+        {
+            public InvoiceMaster InvoiceMaster { get; set; }
+            public IEnumerable<InvoiceDetail> InvoiceDetails { get; set; }
+            //public IEnumerable<UserReportModel> UserReportModels { get; set; }
+        }
+}
---
+++ CRM/Models/DTO/InvoiceDetail.cs
@@ -0,0 +1,23 @@
+﻿namespace CRM.Models.DTO
+{
+    public partial class InvoiceDetail
+    {
+        public int Id { get; set; }
+        public Nullable<int> InvoiceMaster_Id { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> ProductQuantity { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<decimal> FinalPrice { get; set; }
+        public Nullable<decimal> DiscountInPercent { get; set; }
+        public Nullable<decimal> TaxInPercent { get; set; }
+        public string ProductDescription { get; set; }
+        public Nullable<decimal> SGST_TaxInPercent { get; set; }
+        public Nullable<decimal> IGST_TaxInPercent { get; set; }
+        public Nullable<decimal> TaxableAmount { get; set; }
+        public Nullable<decimal> DiscountedAmount { get; set; }
+        public Nullable<decimal> CGSTAmount { get; set; }
+        public Nullable<decimal> SGSTAmount { get; set; }
+        public Nullable<decimal> IGSTAmount { get; set; }
+        public string InvoiceDetail_HSN { get; set; }
+    }
+}
