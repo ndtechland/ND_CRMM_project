898ff3bb7bdabb95127dcd99828a71534292db36
krishnaG111 <ndinfotechteam@gmail.com>
2024-01-17 19:26:45 +0530

    ghjgg

---
+++ CRM/Models/DTO/ECS.cs
@@ -0,0 +1,14 @@
+﻿namespace CRM.Models.DTO
+{
+    public class ECS
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? EmployeeId { get; set; }
+        public string? WorkLocation { get; set; }
+        public long CustomerID { get; set; }
+        public int AccountNumber { get; set; }
+        public string? Ifsc { get; set; }
+        public decimal? netpayment { get; set; }
+    }
+}
