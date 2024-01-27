ec6c30f442d7d173fad8efd9a3d5d0b38454c011
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-01-27 17:38:04 +0530

    akku27

---
+++ CRM/Models/DTO/EPFReportDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CRM.Models.DTO
+{
+    public class EPFReportDTO
+    {
+        public int? Id { get; set; }
+        public string? EmployeeId { get; set; }
+        public string? EmployeeName { get; set; } 
+        public string? PAN { get; set; } 
+        public decimal? MonthlyCtc { get; set; }
+        public List<EPFReportDTO> EPFReports { get; set; }
+    }
+}
