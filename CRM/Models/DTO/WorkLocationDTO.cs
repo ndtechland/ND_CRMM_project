779ae38ea878f8f9b89e6d8093a4a215bdbfd36e
ndshubham <153910931+ndshubham@users.noreply.github.com>
2024-02-17 10:52:44 +0530

    shubu sir

---
+++ CRM/Models/DTO/WorkLocationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CRM.Models.DTO
+{
+    public class WorkLocationDTO
+    {
+        public int Id { get; set; }
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public decimal? Commissoninpercentage { get; set; }
+    }
+}
