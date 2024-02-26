97eb247b596363c0724e23b0b291dd809d7ec4fe
krishnaG111 <ndinfotechteam@gmail.com>
2024-02-26 15:31:31 +0530

    hgg

---
+++ CRM/Models/CRM/Additonalcontribution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class Additonalcontribution
+    {
+        public int Id { get; set; }
+        public string? ContributionName { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool? Isactive { get; set; }
+        public long? CustomerId { get; set; }
+        public long? WorkLocationId { get; set; }
+    }
+}
