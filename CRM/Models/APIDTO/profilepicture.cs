67b757dad0b1b3c58b86027e7c66f19fe0612ffb
krishnaG111 <ndinfotechteam@gmail.com>
2024-02-28 17:54:08 +0530

    ghh

---
+++ CRM/Models/APIDTO/profilepicture.cs
@@ -0,0 +1,8 @@
+﻿namespace CRM.Models.APIDTO
+{
+    public class profilepicture
+    {
+        public string? EmpProfile { get; set; }
+        public IFormFile Empprofilebase64 { get; set; }
+    }
+}
