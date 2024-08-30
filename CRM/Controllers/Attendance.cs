fa508e5b23efd61e2c1bd0ad8358315a44b0f5d0
USER-8 <bob@example.com>
2024-08-30 10:32:54 +0530

    jkgfjklgfmvkmkvjknjkvnjknvjk

---
+++ CRM/Controllers/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    public class Attendance : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult clock()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRM/Models/DTO/salarydetails.cs
@@ -0,0 +1,16 @@
+﻿namespace CRM.Models.DTO
+{
+    public class salarydetails
+    {
+        public decimal Basic { get; set; }
+        public decimal HouseRentAllowance { get; set; }
+        public decimal TravellingAllowance { get; set; }
+        public decimal Esic { get; set; }
+        public decimal Epf { get; set; }
+        public decimal? MonthlyGrossPay { get; set; }
+        public decimal? MonthlyCtc { get; set; }
+        public decimal? Professionaltax { get; set; }
+        public decimal? SpecialAllowance { get; set; }
+        public decimal? Gross { get; set; }
+    }
+}
