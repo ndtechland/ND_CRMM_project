871c83d803efbc24a083c47a703758ba23815525
krishnaG111 <130147991+krishnaG111@users.noreply.github.com>
2024-09-02 00:17:27 -0700

    okkk
    osdhhsd

---
+++ CRM/Models/DTO/ChangePassworddto.cs
@@ -0,0 +1,11 @@
+﻿namespace CRM.Models.DTO
+{
+    public class ChangePassworddto
+    {
+        public string? CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+    }
+}
+//////
