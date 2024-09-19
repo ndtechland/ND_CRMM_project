48e04f6c95958cef5f25f5bf08dc358091f285db
krishnaG111 <ndinfotechteam@gmail.com>
2024-09-19 18:26:01 +0530

    jhdjgdhjgdd

---
+++ CRM/Models/DTO/QuationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CRM.Models.DTO
+{
+    public class QuationDto
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = null!;
+        public string CustomerName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string SalesPersonName { get; set; } = null!;
+        public string[] ProductId { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string[] Amount { get; set; }
+        public string? Mobile { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
