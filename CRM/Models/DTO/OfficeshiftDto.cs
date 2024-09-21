35e8f7289e7b5693918443122bdf8c425aa6f3f6
krishnaG111 <130147991+krishnaG111@users.noreply.github.com>
2024-09-21 10:43:07 +0530

    hiiimadhu

---
+++ CRM/Models/DTO/OfficeshiftDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CRM.Models.DTO
+{
+    public class OfficeshiftDto
+    {
+        public int Id { get; set; }
+        public int? Vendorid { get; set; }
+        public DateTime? Createdate { get; set; }
+        public string? ShiftTypeid { get; set; }
+        public TimeSpan? Starttime { get; set; }
+        public TimeSpan? Endtime { get; set; }
+    }
+}
