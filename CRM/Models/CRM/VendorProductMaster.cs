ef28dd9b73751a6d80e5766c00050055bc34cd84
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-09-24 10:09:56 +0530

    hjygnhj

---
+++ CRM/Models/CRM/VendorProductMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class VendorProductMaster
+    {
+        public int Id { get; set; }
+        public int? VendorId { get; set; }
+        public string? ProductName { get; set; }
+        public int? CategoryId { get; set; }
+        public string? Gst { get; set; }
+        public decimal? ProductPrice { get; set; }
+        public string? Hsncode { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
