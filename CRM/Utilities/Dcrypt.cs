2a9b54ed92d9d5a53cd2691b126b394d7fac92b7
krishnaG111 <130147991+krishnaG111@users.noreply.github.com>
2024-09-24 12:24:04 +0530

    jhfghfjfd

---
+++ CRM/Utilities/Dcrypt.cs
@@ -0,0 +1,42 @@
+﻿using CRM.Models.Crm;
+using Microsoft.AspNetCore.Hosting;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CRM.Utilities
+{
+    public class Dcrypt
+    {
+        public string DecryptPassword(string encodedData)
+        {
+            try
+            {
+                byte[] todecode_bytes = Convert.FromBase64String(encodedData);
+                string decodedString = Encoding.UTF8.GetString(todecode_bytes);
+                return decodedString;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error decoding password: " + ex.Message);
+                return null;
+            }
+        }
+        public string GenerateRandomPassword()
+        {
+            int length = 12;
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=";
+            StringBuilder password = new StringBuilder();
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] buffer = new byte[sizeof(uint)];
+                while (length-- > 0)
+                {
+                    rng.GetBytes(buffer);
+                    uint num = BitConverter.ToUInt32(buffer, 0);
+                    password.Append(validChars[(int)(num % (uint)validChars.Length)]);
+                }
+            }
+            return password.ToString();
+        }
+    }
+}
---
+++ CRM/Utilities/Encrypt.cs
@@ -0,0 +1,25 @@
+﻿using CRM.Models.Crm;
+using Microsoft.AspNetCore.Hosting;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CRM.Utilities
+{
+    public class Encrypt
+    {
+        public string EncryptPassword(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+    }
+}
