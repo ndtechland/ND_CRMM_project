ec424dd25a74c5c2435f95cc64ef53689c58407d
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-05 11:36:28 +0530

    okkk

---
+++ CRM/Models/CRM/CustomerInvoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class CustomerInvoice
+    {
+        public int Id { get; set; }
+        public int? CustomerId { get; set; }
+        public int? VendorId { get; set; }
+        public int? ProductId { get; set; }
+        public decimal? ProductPrice { get; set; }
+        public decimal? RenewPrice { get; set; }
+        public int? NoOfRenewMonth { get; set; }
+        public string? Hsncode { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? RenewDate { get; set; }
+        public decimal? Igst { get; set; }
+        public decimal? Sgst { get; set; }
+        public decimal? Cgst { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
---
+++ CRM/Models/DTO/CustomerInvoiceDTO.cs
@@ -0,0 +1,48 @@
+﻿namespace CRM.Models.DTO
+{
+    public class CustomerInvoiceDTO
+    {
+        public int? ProductId { get; set; }
+        public decimal? ProductPrice { get; set; }
+        public int InvoiceId { get; set; }
+        public int? CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string AlternateNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string ProductName { get; set; }
+        public decimal? RenewPrice { get; set; }
+        public int NoOfRenewMonth { get; set; }
+        public string HsnSacCode { get; set; }
+        public string OfficeState { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCity { get; set; }
+        public string OfficeCity { get; set; }
+        public string OfficeAddress { get; set; }
+        public string BillingAddress { get; set; }
+        public DateTime? InvoiceDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? RenewDate { get; set; }
+        public decimal? IGST { get; set; }
+        public decimal? SGST { get; set; }
+        public decimal? CGST { get; set; }
+        public List<ProductDetail> ProductDetails { get; set; }
+    }
+
+    public class ProductDetail
+    {
+        public int Id { get; set; }
+        public int? ProductId { get; set; }
+        public int? CustomerId { get; set; }
+        public decimal? ProductPrice { get; set; } 
+        public decimal? RenewPrice { get; set; }
+        public int? NoOfRenewMonth { get; set; }
+        public string? HsnSacCode { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? RenewDate { get; set; }
+        public decimal? IGST { get; set; }
+        public decimal? SGST { get; set; }
+        public decimal? CGST { get; set; }
+    }
+     
+}
