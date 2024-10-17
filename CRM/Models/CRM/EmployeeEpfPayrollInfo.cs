e5d8cbe9a7bc24a09d034436f40b685eadbd0e53
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-17 18:14:37 +0530

    okk

---
+++ CRM/Models/CRM/EmployeeEpfPayrollInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class EmployeeEpfPayrollInfo
+    {
+        public int Id { get; set; }
+        public string? EmployeeId { get; set; }
+        public string? Epfnumber { get; set; }
+        public int? Epfpercentage { get; set; }
+        public DateTime? EffectiveDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? Vendorid { get; set; }
+    }
+}
