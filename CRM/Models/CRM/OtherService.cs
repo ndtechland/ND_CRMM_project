2fbd8e3cb06c2c73c64e51c3f5cf27c6f0fcfefa
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-25 16:25:21 +0530

    okkk

---
+++ CRM/Models/CRM/OtherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Models.Crm
+{
+    public partial class OtherService
+    {
+        public int Id { get; set; }
+        public string ServiceName { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
