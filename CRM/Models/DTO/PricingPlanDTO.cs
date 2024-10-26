09d4b8cae037e98c071be7ff3ff4d28d895cd5c6
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-26 16:28:19 +0530

    okkk

--- CRM/Models/DTO/PricingPlanDTO.cs
+++ CRM/Models/DTO/PricingPlanDTO.cs
@@ -7,13 +7,13 @@ namespace CRM.Models.DTO
         public int Id { get; set; }
         public string PlanName { get; set; } = null!;
         public decimal Price { get; set; }
-        public decimal AnnulPrice { get; set; }
-        public decimal AnnulPriceInPercentage { get; set; }
+        public decimal? AnnulPrice { get; set; }
+        public decimal SavePrice { get; set; }
+        public decimal? AnnulPriceInPercentage { get; set; }
         public string? Description { get; set; }
         public string? Image { get; set; }
         public bool? IsActive { get; set; }
         public IFormFile ImageFile { get; set; }
         public DateTime? CreatedDate { get; set; }
-        public List<PricingPlan> PricingPlans { get; set; }
     }
 }
