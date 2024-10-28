befaeb5fe58323bfaae2928d87707b5dfef132db
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-28 14:53:52 +0530

    okkk

---
+++ CRM/Models/DTO/MissionVisionDTO.cs
@@ -0,0 +1,16 @@
+﻿using CRM.Models.Crm;
+
+namespace CRM.Models.DTO
+{
+    public class MissionVisionDTO
+    {
+        public int Id { get; set; }
+        public string? MissionVisionName { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public IFormFile ImageFile { get; set; }
+        public List<MissionVision> MissionVisions { get; set; }
+    }
+}
