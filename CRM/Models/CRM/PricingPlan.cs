1809f4d9d0a7a4db0877dc3ab259a0dad69e4f80
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-10-29 16:06:43 +0530

    okkkk

--- CRM/Models/CRM/PricingPlan.cs
+++ CRM/Models/CRM/PricingPlan.cs
@@ -8,12 +8,11 @@ namespace CRM.Models.Crm
         public int Id { get; set; }
         public string PlanName { get; set; } = null!;
         public decimal Price { get; set; }
-        public string? Description { get; set; }
         public string? Image { get; set; }
         public bool? IsActive { get; set; }
         public DateTime? CreatedDate { get; set; }
         public decimal? AnnulPrice { get; set; }
-        public decimal? AnnulPriceInPercentage { get; set; }
+        public int? AnnulPriceInPercentage { get; set; }
         public string? Title { get; set; }
         public string? Support { get; set; }
     }
