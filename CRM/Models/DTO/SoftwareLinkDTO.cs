be31b93b6d5cbb4cbe8c2d55ae144749e5dcc890
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-12-26 16:05:46 +0530

    code commit

---
+++ CRM/Models/DTO/SoftwareLinkDTO.cs
@@ -0,0 +1,20 @@
+﻿using CRM.Models.Crm;
+
+namespace CRM.Models.DTO
+{
+    public class SoftwareLinkDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public bool IsHeading { get; set; }
+        public bool IsMenu { get; set; }
+        public int? Parent_Id { get; set; }
+        public bool? IsSubHeading { get; set; }
+        public bool? IsSubHeadingTwo { get; set; }
+        public bool? Isvendor { get; set; }
+        public IEnumerable<Softwarelink> SubHeading { get; set; }
+        public IEnumerable<Softwarelink> SubHeadingTwo { get; set; }
+        public IEnumerable<Softwarelink> ChildMenus { get; set; }
+    }
+}
